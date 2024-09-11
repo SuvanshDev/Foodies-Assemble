d942de9c9af5b10c8a4d0069fc9a9072dbf6597c
Suvansh Gupta <suvanshg960@gmail.com>
2024-09-11 23:15:21 +0530

    Initial Boiler Plate

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// Header
+// - Logo
+// - Nav Items
+// Body
+// Footer
+
+const Header = () => {
+    return (
+    <div>
+        <h1>Hi</h1>
+    </div>
+    );
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Header />);
+
