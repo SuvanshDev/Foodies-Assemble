9857c1939fd3270dbe169fbfc2c945aa51210876
Suvansh Gupta <suvanshg960@gmail.com>
2024-10-26 15:10:25 +0530

    Add User(Functional Component) and User Class (Class Based Component) and upto ep8 part 5

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,23 @@
-const About = () => {
+import React from "react";
+import User from "./User";
+import UserClass from "./UserClass";
+class AboutClass extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log('Parent Constructor');
+  }
+  componentDidMount(){
+    console.log('Parent component did mount');
+  }
+  render() {
+    console.log('Parent Render');
     return (
-        <>
+      <>
         <h1>About Us</h1>
         <h2>lorem ipsum lodor</h2>
-        </>
-    )
-
+        <UserClass name={"Sg-classs"} />
+      </>
+    );
+  }
 }
-export default About;
+export default AboutClass;
---
+++ src/components/User.js
@@ -0,0 +1,11 @@
+const User = ({name}) => {
+  return (
+    <div className="user-card">
+      <h3>Name:{name}</h3>
+      <h3>Location: Jammu</h3>
+      <h4>Contact: https://github.com/SuvanshDev</h4>
+    </div>
+  );
+};
+
+export default User;
