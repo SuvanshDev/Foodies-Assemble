a672e2b86c768c956cba28e939441f0330211adb
Suvansh Gupta <suvanshg960@gmail.com>
2024-11-01 11:23:48 +0530

    Create Custom Hook for fetching Restaurant Menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,23 +1,11 @@
-import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API_URL } from "../utils/constant";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
 
   const {resId} = useParams();
-  
-  const [resInfo, setResInfo] = useState(null);
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API_URL+resId);
-    const json = await data.json();
-    setResInfo(json.data);
-  };
-
+  const resInfo = useRestaurantMenu(resId);
   if (resInfo === null) {
     return <Shimmer />;
   }
